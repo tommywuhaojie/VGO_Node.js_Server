a34e9777c357b82baac80bebbfa25e50377be03a
tommywuhaojie <1165637488@qq.com>
2016-05-30 00:01:29 -0700

    add socket.io session support

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
  * Module dependencies.
  */
 var express = require('express');
-var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var connect = require('connect');
 var app = express();
@@ -13,15 +12,18 @@ app.use(express.static(__dirname + '/public'));
 app.use(connect.logger('dev')); 
 app.use(connect.json()); 
 app.use(connect.urlencoded());
-
 app.use(cookieParser());
-app.use(session({
+
+var session = require("express-session")({
     secret: "aacd4d61-b4ee-4790-9faa-48236449b9d6",
     cookie: { maxAge: 7 * 24 * 3600 * 1000 }, // session expires after one week
     proxy: true,
     resave: true,
     saveUninitialized: true
-}));
+});
+var sharedsession = require("express-socket.io-session");
+
+app.use(session);
 
 // Routes  
 require('./routes/routes.js')(app);  
@@ -29,14 +31,10 @@ require('./routes/routes.js')(app);
 var server = app.listen(port);
 console.log('The App runs on port ' + port);
 
-
-// Init Socket.io
+// init Socket.io
 var io = require('socket.io').listen(server);
 
-// Use shared session middleware for socket.io
-// setting autoSave:true
-var sharedsession = require("express-socket.io-session");
-
+// enable socket.io session support
 io.use(sharedsession(session, {
     autoSave:true
 }));
