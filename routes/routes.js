8ec086cf19c8393e50dd99aaefe7b694f839888c
hongyi <hongyili93@gmail.com>
2016-07-05 21:03:43 -0700

    debuging server vertify

--- routes/routes.js
+++ routes/routes.js
@@ -34,7 +34,7 @@ module.exports = function(app) {
 
         var code  = req.body.code;
         var objectid = req.body.objectid;
-
+        console.log('the code enter is '+ code);
         vertify.vertify(code, objectid, function (found) {
             console.log(found);
             res.json(found);
